801e98c54879fd073e693fd0af527901ba14da7f
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2023-08-09 13:49:14 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cronometro");
+        }
+    }
+}
