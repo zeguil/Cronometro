4e397043c87353b5c4283fc83b73d70c681fe6ea
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2023-08-09 15:57:07 -0400

    func start e menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Cronometro
 {
@@ -7,7 +8,57 @@ namespace Cronometro
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Cronometro");
+           
+            Menu();
+        }
+
+        static void Start(int tempo)
+        {
+
+
+            int tempoAtual = 0;
+            while (tempo != tempoAtual)
+            {
+                Console.Clear();
+                tempoAtual++;
+                if (tempoAtual < 60)
+                {
+                    Console.WriteLine("\n" + tempoAtual + "\n");
+                }
+                else
+                {
+                    int minutos = tempoAtual / 60;
+                    int segundos = tempoAtual % 60;
+                    if (segundos < 10)
+                    {
+                        Console.WriteLine($"\n{minutos}:0{segundos}\n");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{minutos}:{segundos}\n");
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("\nFIM DO TEMPO\n");
+            Console.ReadKey();
+        }
+
+        static void Menu()
+        {   
+             Console.WriteLine("\nCRONOMETRO\n");
+            Console.WriteLine("Qual o limite de tempo em segundos?\n\nDigite `0` para sair!!!\n\n");
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            Start(valor);
         }
     }
 }
